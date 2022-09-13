814ff053b75ec8ae160cc938bfce3270c57fb0b7
jerrydav1d <109644211+jerrydav1d@users.noreply.github.com>
2022-09-13 21:15:49 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ class Node:
     """
     Provide necessary documentation
     """
+
     def __init__(self, data=None, next=None):
         """
         Provide necessary documentation
@@ -17,6 +18,7 @@ class LinkedList:
     """
     Provide necessary documentation
     """
+
     def __init__(self):
         """
         Initialize the head
@@ -24,34 +26,74 @@ class LinkedList:
         self.head = None
 
     def insert_at_end(self, data):
+
         """
         Insert node at end of the list
         :param data: integer data that will be used to create a node
         """
-        # Write code here
+        # Writ code here
+        t = self.head
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            while t.next is not None:
+                t = t.next
+            t.next = new_node
 
     def status(self):
         """
         It prints all the elements of list.
         """
         # write code here
+        print("[",end="")
+        temp = self.head
+        while temp.next:
+            print(temp.data,end=", ")
+            temp = temp.next
+        print(f"{temp.data}]",end="\n")
 
 
 class Solution:
     """
     Provide necessary documentation
     """
-    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
+
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
+        LinkedList]:
         """
         :param first_list: Linkedlist with non-negative integers
         :param second_list: Linkedlist with non-negative integers
         :return: returns the sum as a linked list
         """
-        # Write code here
-        
-        
 
-# Do not edit the following code      
+        # Write code here
+        self.first_list = first_list
+        self.second_list = second_list
+
+        dummy = LinkedList()
+        carry = 0
+        temp1, temp2 = first_list.head, second_list.head
+
+        while temp1 or temp2 or carry:
+            v1 = temp1.data if temp1 else 0
+            v2 = temp2.data if temp2 else 0
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            dummy.insert_at_end(val)
+
+
+
+            temp1 = temp1.next if temp1 else None
+            temp2 = temp2.next if temp2 else None
+        return dummy
+
+
+
+
+
+# Do not edit the following code
 # Create an instance for LinkedList
 first_list = LinkedList()
 # Create an another instance for LinkedList
